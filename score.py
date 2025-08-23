4ea1bb89f8831c6488f4db71bab1b4f86d9b4bff
Lê Huy Hoàng <152090257+hoangle2302@users.noreply.github.com>
2025-08-23 22:39:19 +0700

    Update score.py

--- score.py
+++ score.py
@@ -1,14 +1,15 @@
 from turtle import Turtle
 
 class Score(Turtle):
-
-    def __init__(self):
+    def __init__(self, left_player_name, right_player_name):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.left_player_name = left_player_name
+        self.right_player_name = right_player_name
         self.update()
 
     def update(self):
@@ -25,3 +26,14 @@ class Score(Turtle):
     def r_point(self):
         self.r_score += 1
         self.update()
+
+    def check_winner(self):
+        if self.l_score >= 10:
+            self.goto(0, 0)
+            self.write(f"{self.left_player_name} Wins!", align="center", font=("Courier", 40, "bold"))
+            return True
+        elif self.r_score >= 10:
+            self.goto(0, 0)
+            self.write(f"{self.right_player_name} Wins!", align="center", font=("Courier", 40, "bold"))
+            return True
+        return False
